d3aaa1c040b2744a22128c44539276314b177e51
David Bernard <david.bernard.31@gmail.com>
2024-01-28 19:27:29 +0100

    test: add test to validate against jsonschema: examples (from spec), generated samples
    - generate samples with arbitraries (proptest)
    
    Signed-off-by: David Bernard <david.bernard.31@gmail.com>

--- cdevents-sdk/tests/specs.rs
+++ cdevents-sdk/tests/specs.rs
@@ -3,6 +3,8 @@ use cdevents_sdk::CDEvent;
 use rstest::rstest;
 use std::fs;
 use std::path::PathBuf;
+use proptest::prelude::*;
+use boon::{Schemas, Compiler};
 
 #[rstest]
 fn for_each_example(#[files("../cdevents-spec/examples/*.json")] path: PathBuf) {
@@ -26,4 +28,41 @@ fn for_each_example(#[files("../cdevents-spec/examples/*.json")] path: PathBuf) 
     let cdevent_json = serde_json::to_value(cdevent).expect("to convert into json");
     dbg!(&cdevent_json);
     assert_json_eq!(example_json, cdevent_json);
+}
+
+fn check_against_schema(json: &serde_json::Value, ty: &str) {
+    let (subject, predicate) = cdevents_sdk::extract_subject_predicate(ty).expect("valid type: {ty}");
+    let schemapath = format!("../cdevents-spec/schemas/{subject}{predicate}.json");
+    //TODO optimize to not recompile a previously read schema
+    let mut schemas = Schemas::new();
+    let mut compiler = Compiler::new();
+    let sch_index = compiler.compile(&schemapath, &mut schemas);
+    if let Err(err) = sch_index {
+        assert!(false, "{err:#}");
+        return; // to allow sch_index.unwrap()
+    }
+    let sch_index = sch_index.unwrap();
+    let result = schemas.validate(&json, sch_index);
+    if let Err(err) = result {
+        assert!(false, "{err}");
+        return;
+    }
+}
+
+#[rstest]
+fn validate_example_against_schema(#[files("../cdevents-spec/examples/*.json")] path: PathBuf) {
+    let example_txt = fs::read_to_string(path).expect("to read file as string");
+    let example_json: serde_json::Value =
+        serde_json::from_str(&example_txt).expect("to parse as json");
+    let ty = example_json["context"]["type"].as_str().expect("valid context.type in json");
+    check_against_schema(&example_json, ty);
+}
+
+proptest! {
+    #[test]
+    #[cfg(feature = "testkit")]
+    fn arbitraries_check_jsonschema(s in any::<CDEvent>()) {
+        let json = serde_json::to_value(&s).unwrap();
+        check_against_schema(&json, s.ty());
+    }
 }
